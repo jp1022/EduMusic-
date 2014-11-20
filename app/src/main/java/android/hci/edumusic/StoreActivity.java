5093255d3ddda3319db16b27358c523f6cfe59b6
jp1022 <jep1911@umd.edu>
2014-11-20 10:43:05 -0500

    Store Design
    Store menu developed, buttons to buy target instruments included.

--- app/src/main/java/android/hci/edumusic/StoreActivity.java
+++ app/src/main/java/android/hci/edumusic/StoreActivity.java
@@ -1,10 +1,17 @@
 package android.hci.edumusic;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by ian on 11/17/2014.
  */
 public class StoreActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
 
 }
