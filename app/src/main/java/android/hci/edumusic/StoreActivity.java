48928af9582f2b85f1a9dd6dc806e66fa6d5bc9d
jp1022 <jep1911@umd.edu>
2014-11-23 16:16:27 -0500

    Store instruments added
    Structure of the store updated to include all possible instruments.
    Motif still needs work in future update.

--- app/src/main/java/android/hci/edumusic/StoreActivity.java
+++ app/src/main/java/android/hci/edumusic/StoreActivity.java
@@ -1,7 +1,11 @@
 package android.hci.edumusic;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by ian on 11/17/2014.
@@ -14,4 +18,52 @@ public class StoreActivity extends Activity{
         setContentView(R.layout.activity_store);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean goDrum(View v){
+        try{
+            Intent k = new Intent(StoreActivity.this, DrumActivity.class);
+            startActivity(k);
+        } catch(Exception e){
+            //TODO ACTUALLY DO SOMETHING
+        }
+        return true;
+    }
+
+    public boolean goPiano(View v){
+        try{
+            Intent k = new Intent(StoreActivity.this, PianoActivity.class);
+            startActivity(k);
+        } catch(Exception e){
+            //TODO ACTUALLY DO SOMETHING
+        }
+        return true;
+    }
+
+    public boolean goKazoo(View v){
+        try{
+            Intent k = new Intent(StoreActivity.this, KazooActivity.class);
+            startActivity(k);
+        } catch(Exception e){
+            //TODO ACTUALLY DO SOMETHING
+        }
+        return true;
+    }
 }
